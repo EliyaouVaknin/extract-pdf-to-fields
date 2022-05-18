fd248c4f44200a461317c2bce93b42e8d4702345
evdesign <eliyaou5423703@gmail.com>
2022-05-18 10:41:40 +0300

    Version 1

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const pdfParse = require("pdf-parse");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/Resume')
+const port = 3001;
+const app = express();
+app.listen (port, () => console.log (`server is live on port ${port}`));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error: "));
+db.once('open', () => console.log("MongoDB Connected"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/',express.static("public"));
+app.use(fileUpload());
+
+const ResumeSchema = mongoose.Schema({
+    id: String,
+    email: String,
+    linkedIn: String,
+    phone: String,
+    text: String
+})
+
+const Resume = mongoose.model("Resume", ResumeSchema)
+
+app.post("/extract-text", (req,res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    })
+});
+
+app.post("/savedata", async (req, res) => {
+    var myData = new Resume (
+        {
+            id: req.body.id,
+            email: req.body.email,
+            phone: req.body.phone,
+            linkedIn: req.body.linkenInURL,
+            text: req.body.text
+        }
+    )
+    await myData.save()
+    res.json(myData)
+    
+  });
+
+
---
+++ public/Script.js
@@ -0,0 +1,73 @@
+const inputFile = document.getElementById("inputFile");
+const btnUpload = document.getElementById("btnUpload");
+const resultText = document.getElementById("resultText");
+const emailRegex = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/);
+const linkedInRegex = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm);
+const phoneRegex = new RegExp(/\b[\+]?[(]?[0-9]{2,6}[)]?[-\s\.]?[-\s\/\.0-9]{3,15}\b/m);
+const IDRegex = new RegExp(/^\d{9}$/);
+var pdfText = "";
+
+var resume = {
+    id: null,
+    email: null,
+    linkenInURL: null,
+    phone: null,
+    text: null
+}
+
+inputFile.addEventListener("change", () => {
+    const formData = new FormData();
+
+    try {
+        formData.append("pdfFile", inputFile.files[0]);
+    } catch {
+        alert("oops...Error while uploading the file")
+    }
+
+
+    fetch('/extract-text', {
+        method: "POST",
+        body: formData
+    }).then(response => {
+        return response.text();
+    }).then(extractedText => {
+        pdfText = extractedText
+    });
+});
+
+btnUpload.addEventListener("click", () => {
+    if (inputFile.files.length > 0) {
+        try {
+            resume.id = pdfText.match(IDRegex)[0];
+        } catch {
+            resume.id = null
+        }
+        try {
+            resume.email = pdfText.match(emailRegex)[0];
+        } catch {
+            resume.email = null
+        }
+        try {
+            resume.phone = pdfText.match(phoneRegex)[0];
+        } catch {
+            resume.id = null
+        }
+        try {
+            resume.linkenInURL = pdfText.match(linkedInRegex)[0];
+        } catch {
+            resume.linkenInURL = null
+        }
+        resume.text = pdfText;
+        resultText.value = `id:${resume.id}\nemail: ${resume.email}\nphone: ${resume.phone}\nlinkedIn: ${resume.linkenInURL}`;
+
+        fetch('/savedata', {
+            method: 'POST',
+            body: JSON.stringify(resume),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+    } else {
+        alert("Please upload file")
+    }
+});
